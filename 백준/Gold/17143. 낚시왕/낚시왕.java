bcb0a897f2507cf49ab51bc8e9b69ed4cceb8690
앙쏘 <92502057+Ssovely-98@users.noreply.github.com>
2022-10-05 22:29:15 +0900

    [Gold I] Title: 낚시왕, Time: 572 ms, Memory: 47112 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -6,68 +6,63 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int R, C, m;
-	static Shark[][] shark;
-	static int res = 0; // 잡은 상어 크기의 합
+	static int R,C,M;
+	static Shark[][] sea;
+	static int res;
+	
 	static int[] dr= {-1,0,1,0};
 	static int[] dc= {0,-1,0,1};
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str = new StringTokenizer(br.readLine());
-
-		R = Integer.parseInt(str.nextToken());
-		C = Integer.parseInt(str.nextToken());
-		m = Integer.parseInt(str.nextToken());
-
-		shark=new Shark[R][C];
-		for (int i = 0; i < m; i++) {
-			str = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(str.nextToken());
-			int c = Integer.parseInt(str.nextToken());
-			int s = Integer.parseInt(str.nextToken());
-			int d = Integer.parseInt(str.nextToken());
-			int z = Integer.parseInt(str.nextToken());
-			if(d==1)
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str= new StringTokenizer(br.readLine());
+		
+		R=Integer.parseInt(str.nextToken());
+		C=Integer.parseInt(str.nextToken());
+		M=Integer.parseInt(str.nextToken());
+		
+		sea=new Shark[R][C];
+		
+		for(int i=0;i<M;i++) {
+			str=new StringTokenizer(br.readLine());
+			int r=Integer.parseInt(str.nextToken());
+			int c=Integer.parseInt(str.nextToken());
+			int s=Integer.parseInt(str.nextToken());
+			int d=Integer.parseInt(str.nextToken());
+			int z=Integer.parseInt(str.nextToken());
+			if(d==1) {
 				d=0;
-			else if(d==4)
+			}else if(d==4) {
 				d=1;
-			shark[r-1][c-1]=new Shark(r-1, c-1, s, d, z);
+			}
+			//0:상 2:하 3:우1:좌
+			sea[r-1][c-1]=new Shark(r-1, c-1, s, d, z);
 		}
 		
-		for(int i=0;i<C;i++) {
-			//상어 잡기
-			catchShark(i);
+		for(int loc=0;loc<C;loc++) {
+			catchShark(loc);
 			moveShark();
 		}
-		
-		
 		System.out.println(res);
 	}
-
-
+	
 	private static void moveShark() {
-		// 상어 이동
 		Queue<Shark> q=new LinkedList<>();
 		for(int i=0;i<R;i++) {
 			for(int j=0;j<C;j++) {
-				if(shark[i][j]!=null)
-					q.offer(new Shark(i,j,shark[i][j].s,shark[i][j].d,shark[i][j].z));
+				if(sea[i][j]!=null) {
+					q.offer(sea[i][j]);
+				}
 			}
 		}
-		
-		shark=new Shark[R][C];
+		sea=new Shark[R][C];
 		
 		while(!q.isEmpty()) {
 			Shark tmp=q.poll();
-			
 			int speed=tmp.s;
-			
 			if(tmp.d==0||tmp.d==2) {
-				//상 ,하
 				speed%=(R-1)*2;
-			}else if(tmp.d==3||tmp.d==1) {
-				//좌 우
+			}else if(tmp.d==1||tmp.d==3) {
 				speed%=(C-1)*2;
 			}
 			
@@ -76,7 +71,6 @@ public class Main {
 				int cc=tmp.c+dc[tmp.d];
 				
 				if(rr<0||rr>=R||cc<0||cc>=C) {
-					//벽에 부딪이면
 					tmp.r-=dr[tmp.d];
 					tmp.c-=dc[tmp.d];
 					tmp.d=(tmp.d+2)%4;
@@ -86,35 +80,32 @@ public class Main {
 				tmp.r=rr;
 				tmp.c=cc;
 			}
-			//System.out.println(tmp);
-			if(shark[tmp.r][tmp.c]!=null) {
-				if(shark[tmp.r][tmp.c].z<tmp.z) {
-					shark[tmp.r][tmp.c]=new Shark(tmp.r, tmp.c, tmp.s, tmp.d, tmp.z);
+			if(sea[tmp.r][tmp.c]!=null) {
+				if(sea[tmp.r][tmp.c].z<tmp.z) {
+					sea[tmp.r][tmp.c]=tmp;
 				}
 			}else {
-				shark[tmp.r][tmp.c]=new Shark(tmp.r, tmp.c, tmp.s, tmp.d, tmp.z);
-			}
-		}
-	}
-
-
-	private static void catchShark(int idx) {
-		// 상어 잡기
-		for(int i=0;i<R;i++) {
-			if(shark[i][idx]!=null) {
-				//상어가 존재한다면
-				res+=shark[i][idx].z;
-				//System.out.println(shark[i][idx].z);
-				shark[i][idx]=null;
-				return;
+				sea[tmp.r][tmp.c]=tmp;
 			}
 		}
 		
 	}
 
+	private static void catchShark(int loc) {
+		// 상어 잡기
+		for(int i=0;i<R;i++) {
+			if(sea[i][loc]!=null) {
+				res+=sea[i][loc].z;
+				//System.out.println(res);
+				sea[i][loc]=null;
+				//한 마리만 먹기
+				return;
+			}
+		}		
+	}
 
-	static class Shark {
-		int r, c, s, d, z;
+	static class Shark{
+		int r,c,s,d,z;
 
 		public Shark(int r, int c, int s, int d, int z) {
 			this.r = r;
@@ -122,11 +113,6 @@ public class Main {
 			this.s = s;
 			this.d = d;
 			this.z = z;
-		}
-
-		@Override
-		public String toString() {
-			return "Shark [r=" + r + ", c=" + c + ", s=" + s + ", d=" + d + ", z=" + z + "]";
-		}
+		}		
 	}
-}
+}	
