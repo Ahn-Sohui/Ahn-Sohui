8938027f0cf3a352d7ede5cce18f738fb28528af
앙쏘 <92502057+Ssovely-98@users.noreply.github.com>
2022-11-04 00:35:11 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 20212 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,40 +3,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static StringBuilder sb= new StringBuilder();
+	static int n,m;
+	static int[] sel;
+	static boolean[] v;
+	static StringBuilder sb=new StringBuilder();
+	
 	public static void main(String[] args) throws Exception{
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str= new StringTokenizer(br.readLine());
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str=new StringTokenizer(br.readLine());
 		
-		int n=Integer.parseInt(str.nextToken());
-		int m=Integer.parseInt(str.nextToken());
-
-		int[] sel=new int[m];
+		n=Integer.parseInt(str.nextToken());
+		m=Integer.parseInt(str.nextToken());
+		sel=new int[m];
+		v=new boolean[n];
+		recur(0);
 		
-		boolean[] visited=new boolean[n];
-		
-		recur(n,sel,visited,0);
 		System.out.println(sb);
-		
 	}
-	private static void recur(int n,int[] sel, boolean[] visited, int idx) {
-		if(idx==sel.length) {
-			for(int i=0;i<sel.length;i++) {
+	
+	private static void recur(int depth) {
+		if(depth==m) {
+			for(int i=0;i<m;i++) {
 				sb.append(sel[i]).append(" ");
 			}
 			sb.append("\n");
+			
 			return;
 		}
 		
 		for(int i=0;i<n;i++) {
-			if(!visited[i]) {
-				visited[i]=true;
-				sel[idx]=i+1;
-				recur(n,sel,visited,idx+1);
-				visited[i]=false;
+			if(!v[i]) {
+				v[i]=true;
+				sel[depth]=i+1;
+				recur(depth+1);
+				v[i]=false;
 			}
 		}
-		
 	}
 
 }
